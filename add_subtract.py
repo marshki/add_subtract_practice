6e907091258d795552f680a89fa417f18e12b4b1
marshki <marshall@morethantomorrow.org>
2016-07-27 15:13:20 -0400

    added placeholder for program; overview of program

---
+++ add_subtract.py
@@ -0,0 +1,7 @@
+#!/bin/py
+
+print(""" 
+	Text-based program for practicing addition and subtraction problems under timed conditions and for a score. 
+    	There's some overlap with multiplication_practice.py in another repo, so I'll be recycling some of the code 
+	from there for this project. In the future, it's possible that the two programs will merge. 
+""")
